4140e887ed34632477c64bced9a16eadd4f82733
MarioMedhat10 <mariomedhat4g@gmail.com>
2024-03-06 00:07:41 +0200

    Added Search icon in appbar and make it navigate to SearchScreen

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:weather_app/screens/no_weather_screen.dart';
+import 'package:weather_app/screens/search_screen.dart';
 import 'package:weather_app/screens/weather_info_screen.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -9,14 +10,28 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(
+        backgroundColor: Colors.blue,
+        foregroundColor: Colors.white,
+        title: const Text(
           "Weather App",
           style: TextStyle(
             fontWeight: FontWeight.w600,
           ),
         ),
-        backgroundColor: Colors.blue,
-        foregroundColor: Colors.white,
+        actions: [
+          IconButton(
+            onPressed: () {
+              Navigator.of(context).push(
+                MaterialPageRoute(
+                  builder: (context) => SearchScreen(),
+                ),
+              );
+            },
+            icon: const Icon(
+              Icons.search,
+            ),
+          ),
+        ],
       ),
       body: WeatherInfoScreen(),
     );

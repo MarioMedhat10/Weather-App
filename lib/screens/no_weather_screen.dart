00828e7b28b72d84c26089d12b56f14c8f089753
MarioMedhat10 <mariomedhat4g@gmail.com>
2024-03-05 22:42:32 +0200

    created NoWeatherScreen

---
+++ lib/screens/no_weather_screen.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+class NoWeatherScreen extends StatelessWidget {
+  const NoWeatherScreen({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(
+            'There is no weather 😔',
+            style: TextStyle(
+              fontSize: 25,
+            ),
+          ),
+          Text(
+            'Start searching Now 🔍',
+            style: TextStyle(
+                fontSize: 25,
+                fontWeight: FontWeight.bold
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}

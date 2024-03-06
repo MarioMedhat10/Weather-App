7dbbb114c13ae35960fa81f7145a727a2396f2a6
MarioMedhat10 <mariomedhat4g@gmail.com>
2024-03-07 00:25:20 +0200

    used DioException and handled error responses

--- lib/services/weather_service.dart
+++ lib/services/weather_service.dart
@@ -1,16 +1,34 @@
+import 'dart:developer';
+
 import 'package:dio/dio.dart';
 import 'package:weather_app/constants/api_key.dart';
 import 'package:weather_app/models/weather_model.dart';
 
 class WeatherService {
-  final Dio dio ;
+  final Dio dio;
+
   final String baseUrl = 'https://api.weatherapi.com/v1';
 
   WeatherService(this.dio);
-  Future<WeatherModel> getCurrentWeather({required String cityName}) async {
-    Response response = await dio.get('$baseUrl/forecast.json?key=$apiKey&q=$cityName&days=1');
 
-    WeatherModel weatherModel = WeatherModel.fromJson(response.data);
-    return weatherModel;
+  Future<WeatherModel> getCurrentWeather({required String cityName}) async {
+    /*
+    * Dio has a good way for handling exceptions
+    * it can be used throw try catch by using on DioException
+    * */
+    try {
+      Response response = await dio
+          .get('$baseUrl/forecast.json?key=$apiKey&q=$cityName&days=1');
+
+      WeatherModel weatherModel = WeatherModel.fromJson(response.data);
+      return weatherModel;
+    } on DioException catch (e) {
+      final String errorMessage = e.response?.data["error"]["message"] ??
+          'oops there was an error, try later';
+      throw Exception(errorMessage);
+    } catch (e) {
+      log(e.toString());
+      throw Exception('oops there was an error, try later');
+    }
   }
 }

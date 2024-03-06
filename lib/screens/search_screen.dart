fde972e5006078ff13f1b9edc3780ad4b2b53edb
MarioMedhat10 <mariomedhat4g@gmail.com>
2024-03-06 13:57:23 +0200

    created SearchScreen

---
+++ lib/screens/search_screen.dart
@@ -0,0 +1,99 @@
+import 'dart:developer';
+
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+class SearchScreen extends StatelessWidget {
+  const SearchScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.blue,
+        foregroundColor: Colors.white,
+        systemOverlayStyle: const SystemUiOverlayStyle(
+          statusBarIconBrightness: Brightness.light,
+        ),
+        title: const Text(
+          'Search a City',
+          style: TextStyle(fontWeight: FontWeight.w600),
+        ),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 16),
+        child: Center(
+          child: TextField(
+            // to customize the cursor color
+            cursorColor: Colors.blue,
+            /*
+            * to access the value inside text field
+            *   there are two ways :
+            *   - using onChange -> and here we can use log from developer library to see it
+            *                       used when you want to track the changes of text typing
+            *   - using onSubmitted -> used to send the final value typed
+            * */
+            // onChanged: (value) {
+            //   log(value);
+            // },
+            onSubmitted: (value) {
+              log(value);
+            },
+            decoration: InputDecoration(
+              // border: OutlineInputBorder(
+              //   borderRadius: BorderRadius.circular(16),
+              //   borderSide: BorderSide(
+              //     color: Colors.white
+              //   )
+              // ),
+              /*
+              * [enabledBorder] is used to customize the border in its inactive state
+              * */
+              enabledBorder: OutlineInputBorder(
+                // borderRadius: BorderRadius.circular(16),
+                borderSide: BorderSide(
+                  color: Colors.blue,
+                  width: 3,
+                ),
+              ),
+              /*
+              * [enabledBorder] is used to customize the border in its active state
+              * */
+              focusedBorder: OutlineInputBorder(
+                // borderRadius: BorderRadius.circular(16),
+                borderSide: BorderSide(
+                  color: Colors.blue,
+                  width: 3,
+                ),
+              ),
+              // to add hint text inside input field
+              hintText: 'Enter city name:',
+              // hintStyle: TextStyle(
+              //   color: Colors.blue,
+              // ),
+              /*
+              * to add icons there is two ways :
+              * prefixIcon -> icon appear in the left of input field
+              * suffixIcon -> icon appear in the right of input field
+              * */
+              suffixIcon: Icon(
+                Icons.search,
+              ),
+              suffixIconColor: Colors.blue,
+              labelText: 'Search',
+              labelStyle: TextStyle(color: Colors.blue),
+              /*
+              * to customize the width and height of text input field
+              *  we need to use content padding
+              * */
+              contentPadding: EdgeInsets.symmetric(
+                vertical: 28,
+                horizontal: 16,
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
